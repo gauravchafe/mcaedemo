eaf469813133add018b312a2b64c7a1ad8536048
gauravchafe <gaurav.chafe@gmail.com>
2025-02-07 18:45:45 +0530

    Pardot Snippet Added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-piAId = '993542';
-piCId = '284912';
-piHostname = 'pi.demo.pardot.com';
-
-(function() {
-    function async_load(){
-        var s = document.createElement('script'); s.type = 'text/javascript';
-        s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
-        var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
-    }
-    if(window.attachEvent) { window.attachEvent('onload', async_load); }
-    else { window.addEventListener('load', async_load, false); }
-})();
-
 (function ($) {
     "use strict";
 
